b717999edde1900d4d54e5616665ddb5fb36d169
raftarravi <ranjan.ravi1204@gmail.com>
2025-07-24 15:20:45 +0530

    this is our fourth commit with updated ui/ux

--- src/main/java/com/torrentmedia/torrentmedia/service/MailService.java
+++ src/main/java/com/torrentmedia/torrentmedia/service/MailService.java
@@ -6,7 +6,6 @@ import com.torrentmedia.torrentmedia.entity.SubmitContactUs;
 import com.torrentmedia.torrentmedia.repository.ContactUs;
 import com.torrentmedia.torrentmedia.repository.NewsLetterRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,8 +15,7 @@ public class MailService {
     private ContactUs contactUs;
     @Autowired
     private NewsLetterRepo newsLetterRepo;
-    @Autowired
-    private JavaMailSender mailSender;
+
 
 
     public String newsletterMail(NewsLetter newsLetter) {
@@ -32,21 +30,6 @@ public class MailService {
     }
 
     public void getInTouchWithUs(ContactUsForm contactUsForm){
-
-//        SimpleMailMessage userMessage = new SimpleMailMessage();
-//        userMessage.setTo(contactUsForm.getEmail()); // Your inbox
-//        userMessage.setSubject("confirmation mail");
-//        userMessage.setText("Thank You For Contactin Us \n We will get Back to you Very Shortly.");
-//
-//        mailSender.send(userMessage);
-//
-//        SimpleMailMessage adminMessage = new SimpleMailMessage();
-//        adminMessage.setTo("doctoappointment14@gmail.com");
-//        adminMessage.setSubject("New User Want to Talk.");
-//        adminMessage.setText("Details of The User \n" + "UserName : " + contactUsForm.getName() + "\n Email : "+ contactUsForm.getEmail() + "\n Contact : " + contactUsForm.getPhone()+ "\n Message : " + contactUsForm.getSubject() +"\n" + contactUsForm.getMessage() + " . \n Thank You ");
-//
-//        mailSender.send(adminMessage);
-
 
     }
 }
